65ace9560c5b899e72a160d89156a59c592c9a3b
root <root@localhost.localdomain>
2016-08-03 11:13:33 -0400

    started on login code

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "corpus1";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT hash from users where username = :name");
+    $stmt->bindParam(':name', $loginname);
+    $loginname = $_POST["username"];
+    echo $loginname;
+    $stmt->execute();
+    if($stmt->fetchColumn() === $_POST["password"]) {
+
+    }
+    $conn = null;
+} catch(PDOException $e) {
+    echo "Database connection failed, please go back ?>and try again.<br>Debug Info:<br>" . $e->getMessage();
+}
+?>
