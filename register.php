bc838aabd5dfcd28262c3c047ca38468a9de796b
root <root@localhost.localdomain>
2016-08-05 08:40:33 -0400

    yay!

--- register.php
+++ register.php
@@ -7,13 +7,13 @@ $dbname = "corpus1";
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT hash from users where username = :name");
+    $stmt = $conn->prepare("SELECT COUNT(*) from users where username = :name");
     $stmt->bindParam(':name', $loginname);
     $loginname = $_POST["username"];
     //echo $loginname;
     $stmt->execute();
-    if($stmt->fetchColumn() === $_POST["password"]) {
-        echo "Password Correct!";
+    if($stmt->fetchColumn() > 0) {
+        echo "ERROR Username already exists"
     }
     $conn = null;
 } catch(PDOException $e) {
